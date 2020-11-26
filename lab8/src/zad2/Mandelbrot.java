eb110e1d78d00fcc316d6503ab189faed8d58cf2
Rivit <albert98g@gmail.com>
2020-11-26 22:44:16 +0100

    executors tests - lab8

--- lab8/src/zad2/Mandelbrot.java
+++ lab8/src/zad2/Mandelbrot.java
@@ -1,2 +1,62 @@
-package zad2;public class Mandelbrot {
+package zad2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+class Mandelbrot extends JFrame {
+    private final int maxIter;
+    private final BufferedImage img;
+    private final int width;
+    private final int height;
+    private long timeOfExecution;
+
+    public Mandelbrot(int max_iter) {
+        super("Mandelbrot Set");
+        setBounds(100, 100, 800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.maxIter = max_iter;
+        this.width = getWidth();
+        this.height = getHeight();
+        this.img = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    }
+
+    public void run(ExecutorService executorService) {
+        var start = System.currentTimeMillis();
+
+        var futures = new LinkedList<Future<Void>>();
+        for (int y = 0; y < height; y++) {
+            var task = new MandelbrotWorker(maxIter, width, y, img);
+            futures.add(executorService.submit(task));
+        }
+
+        for (var f : futures) {
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        timeOfExecution = System.currentTimeMillis() - start;
+    }
+
+    public long getTimeOfExecution() {
+        return timeOfExecution;
+    }
+
+    public void display() {
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, this);
+    }
 }
--- lab8/src/zad2/MandelbrotWorker.java
+++ lab8/src/zad2/MandelbrotWorker.java
@@ -1,2 +1,39 @@
-package zad2;public class MandelbrotWorker {
+package zad2;
+
+import java.awt.image.BufferedImage;
+import java.util.concurrent.Callable;
+
+class MandelbrotWorker implements Callable<Void> {
+    private final double ZOOM = 150;
+
+    private final int iters;
+    private final int w;
+    private final int current_h;
+    private final BufferedImage img;
+
+    public MandelbrotWorker(int iters, int w, int current_h, BufferedImage img) {
+        this.iters = iters;
+        this.w = w;
+        this.current_h = current_h;
+        this.img = img;
+    }
+
+    @Override
+    public Void call() {
+        double cY = (current_h - 300) / ZOOM;
+        for (int x = 0; x < w; x++) {
+            double zy = 0;
+            double zx = 0;
+            double cX = (x - 400) / ZOOM;
+            int iter = iters;
+            while (zx * zx + zy * zy < 4 && iter > 0) {
+                double tmp = zx * zx - zy * zy + cX;
+                zy = 2.0 * zx * zy + cY;
+                zx = tmp;
+                iter--;
+            }
+            img.setRGB(x, current_h, iter | (iter << 8));
+        }
+        return null;
+    }
 }
