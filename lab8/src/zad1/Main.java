035dbaad3de435bff1a7e043b738c0401a64f4db
Rivit <albert98g@gmail.com>
2020-11-26 22:43:27 +0100

    mandelbrot set

--- lab8/src/zad1/Main.java
+++ lab8/src/zad1/Main.java
@@ -1,2 +1,16 @@
-package zad1;public class Main {
+package zad1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.concurrent.*;
+
+class Main {
+    public static void main(String[] args) {
+        var mandelbrot = new Mandelbrot(570);
+        var executor = Executors.newFixedThreadPool(4);
+        mandelbrot.run(executor);
+        mandelbrot.display();
+    }
 }
--- lab8/src/zad1/Mandelbrot.java
+++ lab8/src/zad1/Mandelbrot.java
@@ -1,2 +1,53 @@
-package zad1;public class Mandelbrot {
+package zad1;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.LinkedList;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+
+class Mandelbrot extends JFrame {
+    private final int maxIter;
+    private final BufferedImage img;
+    private final int width;
+    private final int height;
+
+    public Mandelbrot(int max_iter) {
+        super("Mandelbrot Set");
+        setBounds(100, 100, 800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.maxIter = max_iter;
+        this.width = getWidth();
+        this.height = getHeight();
+        this.img = new BufferedImage(getWidth(), height, BufferedImage.TYPE_INT_RGB);
+    }
+
+    public void run(ExecutorService executorService) {
+        var futures = new LinkedList<Future<Void>>();
+        for (int y = 0; y < height; y++) {
+            var task = new MandelbrotWorker(maxIter, width, y, img);
+            futures.add(executorService.submit(task));
+        }
+
+        for (var f : futures) {
+            try {
+                f.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void display() {
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(img, 0, 0, this);
+    }
 }
